de2c069aff9627e57133490f7a039a5ec6b8c49f
Jesy <jncepero@gmail.com>
2021-06-08 16:53:31 +0200

    creación y maquetación de formulario producto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { FormularioProductoComponent } from './components/formulario-producto/formulario-producto.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
   /* { path: 'productos' }, */
+  { path: 'form', component: FormularioProductoComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroUsuarioComponent },
