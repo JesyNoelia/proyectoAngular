e6126bd02e82f18a6c7835862f9baf8a973a9daf
Jesy <jncepero@gmail.com>
2021-06-10 17:58:08 +0200

    background y modificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'colesAngular';
+
+  public path: string;
+
+  constructor(private activate: ActivatedRoute, private router: Router) {
+    router.events.subscribe((val) => {
+      // see also 
+      if (val instanceof NavigationEnd) {
+        this.path = this.router.url;
+
+      }
+      //avisa cuando se cambia una ruta
+    });
+  }
+
+  getImage() {
+    //console.log(this.path);
+
+    switch (this.path) {
+      case '/form':
+        return '../../../assets/imagenes/libroabierto.jpg';
+      case '/login':
+        return '../../../assets/imagenes/libroclip2.jpg';
+      case '/registro':
+        return '../../../assets/imagenes/lapizamarillo.jpg';
+
+
+    }
+  }
 }
