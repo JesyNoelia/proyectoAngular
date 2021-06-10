b2f41979689bbb401d8ba0ee36fe57b951f1c1c1
Alicia <aliciapradag@gmail.com>
2021-06-10 16:25:38 +0200

    Réplica de Cards en Home

--- src/app/components/card-home/card-home.component.ts
+++ src/app/components/card-home/card-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-home',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardHomeComponent implements OnInit {
 
-  constructor() { }
+  @Input() titulo: string;
+  @Input() texto: string;
+  @Input() color: string;
+  @Input() imagen: string;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
