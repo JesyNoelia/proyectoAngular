b1fc600231f2c5c9a0e9ccad4044f068e25b1bea
Alicia <aliciapradag@gmail.com>
2021-06-14 09:55:04 +0200

    Creación componente mapas

---
+++ src/app/components/mapa-coles/mapa-coles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-coles',
+  templateUrl: './mapa-coles.component.html',
+  styleUrls: ['./mapa-coles.component.css']
+})
+export class MapaColesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
