42168bf6fc51c6fd635881a354b9fc0b7b9cbf77
Jesy <jncepero@gmail.com>
2021-06-14 20:33:53 +0200

    modificación título de formularios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CardProductoComponent } from './components/card-producto/card-producto.component';
 import { CarritoComponent } from './components/carrito/carrito.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { FormularioProductoComponent } from './components/formulario-producto/formulario-producto.component';
 import { HomeComponent } from './components/home/home.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 import { LoginComponent } from './components/login/login.component';
 import { MapaColesComponent } from './components/mapa-coles/mapa-coles.component';
 import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
@@ -13,7 +13,7 @@ import { RegistroUsuarioComponent } from './components/registro-usuario/registro
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
-  { path: 'productos', component: CardProductoComponent },
+  { path: 'productos', component: ListaProductosComponent },
   { path: 'form', component: FormularioProductoComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'login', component: LoginComponent },
