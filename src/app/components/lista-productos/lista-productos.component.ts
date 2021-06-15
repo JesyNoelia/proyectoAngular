faa236a8deb273467a9fbe361683f6e132fabea5
Jesy <jncepero@gmail.com>
2021-06-15 20:56:24 +0200

    maquetación lista productos y métodos

--- src/app/components/lista-productos/lista-productos.component.ts
+++ src/app/components/lista-productos/lista-productos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from 'src/app/interfaces/producto.interface';
 import { ProductoService } from 'src/app/services/producto.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-lista-productos',
@@ -9,44 +10,54 @@ import { ProductoService } from 'src/app/services/producto.service';
 })
 export class ListaProductosComponent implements OnInit {
 
-  productos: Producto[];
+  arrProductos: Producto[];
   search: string;
   currentPage: number;
 
+
   constructor(private productoService: ProductoService) {
 
-    this.productos = [];
+
     this.search = "";
     this.currentPage = 1;
   }
 
   ngOnInit(): void {
     this.productoService.getAll()
-      .then(response => { console.log(response) }
-      )
+      .then(response => {
+        this.arrProductos = response
+        //console.log(this.arrProductos);
+      })
+
       .catch(error => console.log(error));
   }
 
-  onChange($event) {
-    /* if ($event.target.value === "") {
-      this.productos = this.productoService.getAll();
+  async onChange($event) {
+    if ($event.target.value === "") {
+      this.arrProductos = await this.productoService.getAll();
     } else {
-      this.productos = this.productoService.getByCategoria($event.target.value)
-    } */
+      this.arrProductos = await this.productoService.getByCategoria($event.target.value)
+    }
   }
 
-  onClick() {
-    this.productos = this.productoService.getByWord(this.search)
-    console.log(this.search);
-
+  async onClick() {
+    if (this.search === "") {
+      this.arrProductos = await this.productoService.getAll();
+    } else {
+      Swal.fire('Lo sentimos', 'Los criterios de búsqueda no coinciden con los productos disponibles', 'error')
+      this.arrProductos = await this.productoService.getByWord(this.search)
+      if (this.arrProductos.length === 0) {
+        this.arrProductos = await this.productoService.getAll();
+      }
+    }
   }
 
   onClickBtn(siguiente: boolean) {
     this.currentPage = siguiente ? (this.currentPage + 1) : (this.currentPage - 1);
+    console.log(this.currentPage);
 
     this.productoService.getAll(this.currentPage)
-      .then(response => this.productos = response)
+      .then(response => this.arrProductos = response)
       .catch(error => console.log(error))
   };
-
 }
