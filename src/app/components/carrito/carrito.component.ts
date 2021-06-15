52104bfb570bea4022731e28d7f22138828a025b
Jesy <jncepero@gmail.com>
2021-06-15 20:57:17 +0200

    carrito component y métodos, con maquetación

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Producto } from 'src/app/interfaces/producto.interface';
 import { ProductoService } from 'src/app/services/producto.service';
@@ -10,14 +11,35 @@ import { ProductoService } from 'src/app/services/producto.service';
 })
 export class CarritoComponent implements OnInit {
 
-  productos: Producto[]
+  productos: Producto[];
+  private baseUrl: string;
 
-  constructor(private productoService: ProductoService) {
+
+  constructor(private productoService: ProductoService, private httpClient: HttpClient) {
+    this.baseUrl = 'http://localhost:3000/api/pedidos'
     this.productos = []
   }
 
-  ngOnInit() {
-    this.productos = this.productoService.getCart();
+  async ngOnInit() {
+    this.productos = await this.productoService.getAll();
+  }
+
+  async onClickComprar() {
+
+    //const carritoLocal = JSON.parse(localStorage.getItem('carrito'))
+    //return this.httpClient.post(`${this.baseUrl}`).toPromise();
+    const response = await this.productoService.getCart();
+    console.log(response);
+    const vaciarCarrito = await this.productoService.clearCart();
+
+  }
+  onClickVaciar() {
+    this.productoService.clearCart();
+
+  }
+
+  onClickEliminar(pProducto) {
+
   }
 
 }
