e4db5440a976f764a6e42a4d11a2084112216377
Jesy <jncepero@gmail.com>
2021-06-23 09:22:19 +0200

    nuevo componente favoritos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { PerfilUsuarioComponent } from './components/perfil-usuario/perfil-usuar
 import { DetalleProductoComponent } from './components/detalle-producto/detalle-producto.component';
 import { ConocenosComponent } from './components/conocenos/conocenos.component';
 import { ModificarProductoComponent } from './components/modificar-producto/modificar-producto.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
 
 
 
@@ -44,6 +45,7 @@ import { ModificarProductoComponent } from './components/modificar-producto/modi
     DetalleProductoComponent,
     ConocenosComponent,
     ModificarProductoComponent,
+    FavoritosComponent,
 
 
   ],
